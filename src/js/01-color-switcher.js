eb570af915bb0e33eb5f5cdcd72420bea62404f8
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-01-09 16:00:36 +0100

    Finished task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let intervalId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function handleStart() {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+function handleStop() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', handleStart);
+stopBtn.addEventListener('click', handleStop);
