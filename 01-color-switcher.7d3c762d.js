79591a2aa3f3a3de650b578a5cf47f84cde299bd
rafalszewczyk890 <109361257+rafalszewczyk890@users.noreply.github.com>
2023-01-09 18:04:23 +0000

    Deploying to gh-pages from @ rafalszewczyk890/goit-js-hw-09@7fec4f03a367017646b50e33c2503cd216237534 🚀

---
+++ 01-color-switcher.7d3c762d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");let o=null;e.addEventListener("click",(function(){o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(o),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.7d3c762d.js.map
