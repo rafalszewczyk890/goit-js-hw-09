6349116f81b3770d1e5b81974d2b636d4e59c8a0
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-01-10 14:31:57 +0100

    Most of the task 02 done - timer now working

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,7 +2,14 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
+let chosenTime = null;
 const startBtn = document.querySelector('button[data-start]');
+
+const daySpan = document.querySelector('span[data-days]');
+const hourSpan = document.querySelector('span[data-hours]');
+const minuteSpan = document.querySelector('span[data-minutes]');
+const secondSpan = document.querySelector('span[data-seconds]');
+
 const timeInput = document.querySelector('#datetime-picker');
 const options = {
   enableTime: true,
@@ -10,15 +17,42 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    chosenTime = selectedDates[0];
     if (selectedDates[0] <= new Date()) {
       Notiflix.Notify.warning('Please choose a date in the future');
       startBtn.disabled = true;
     } else {
       startBtn.disabled = false;
     }
-    console.log(selectedDates[0]);
   },
 };
 
-startBtn.disabled = true;
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function counterStart() {
+  let timeLeft = chosenTime - new Date();
+  setInterval(() => {
+    let convertedTime = convertMs((timeLeft -= 1000));
+    daySpan.textContent = convertedTime.days;
+    hourSpan.textContent = convertedTime.hours;
+    minuteSpan.textContent = convertedTime.minutes;
+    secondSpan.textContent = convertedTime.seconds;
+  }, 1000);
+}
+
 flatpickr(timeInput, options);
+
+startBtn.disabled = true;
+startBtn.addEventListener('click', counterStart);
